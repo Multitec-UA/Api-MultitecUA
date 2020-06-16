b922508400256b7f075cbbe9c128a207d6a44a6b
MCheca <marcoscheca1@hotmail.com>
2020-06-16 19:23:26 +0200

    Primera versión

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const functions = require('firebase-functions');
+let admin = require("firebase-admin");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({
+    origin: true
+}));
+
+let serviceAccount = require("./credentials.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://multitecapi.firebaseio.com"
+});
+const db = admin.firestore();
+
+app.get('/hello-world', (req, res) => {
+    return res.status(200).send('Hello World!');
+});
+
+// create
+app.post('/api/create', (req, res) => {
+    console.log(req.body);
+    (async () => {
+        try {
+            await db.collection('items').doc('/' + req.body.id + '/')
+                .create({
+                    item: req.body.item
+                });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+exports.app = functions.https.onRequest(app);
