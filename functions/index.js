a56bd2d0bd81606b2e5aa1bfc072757027be86d9
imharvol <imharvol@gmail.com>
2020-06-20 12:08:09 +0200

    Simplificar async

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 const functions = require('firebase-functions');
-let admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 app.use(cors({
     origin: true
 }));
 
-let serviceAccount = require("./credentials.json");
+const serviceAccount = require("./credentials.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -27,18 +28,16 @@ app.get('/hello-world', (req, res) => {
  * @description
  * Crea un miembro a partir de los datos introducidos.
  */
-app.post('/api/crear-miembro', (req, res) => {
+app.post('/api/crear-miembro', async (req, res) => {
     console.log(req.body);
-    (async () => {
-        try {
-            await db.collection('miembros').doc('/' + req.body.numero_socio + '/')
-                .create(req.body);
-            return res.status(200).json({ status:'ok' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({ status:'error', error: error });
-        }
-    })();
+    try {
+        await db.collection('miembros').doc('/' + req.body.numero_socio + '/')
+            .create(req.body);
+        return res.status(200).json({ status:'ok' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status:'error', error: error });
+    }
 });
 
 /**
@@ -47,73 +46,66 @@ app.post('/api/crear-miembro', (req, res) => {
  */
 app.get('/api/get-miembros/:idMiembro', (req, res) => {
     //console.log(req.params.idMiembro);
-    let miembros = db.collection('miembros').doc(req.params.idMiembro);
-    let getDoc = miembros.get()
-        .then(doc => {
-            if (!doc.exists) {
-                console.log('No such document!');
-                return res.status(500).json({ status:'error', error: error });
-            } else {
-                console.log('Document data:', doc.data());
-                return res.status(200).json({ status:'ok', data: doc.data() });
-            }
-        })
-        .catch(err => {
-            console.log('Error getting document', err);
-        });
+    try {
+        const miembros = db.collection('miembros').doc(req.params.idMiembro);
+        const doc = await miembros.get()
+        if (!doc.exists) {
+            console.log('No such document!');
+            return res.status(500).json({ status:'error', error: error });
+        } else {
+            console.log('Document data:', doc.data());
+            return res.status(200).json({ status:'ok', data: doc.data() });
+        }
+    } catch (err) {
+        console.log('Error getting document', err);
+    }
 });
 
 /**
  * @description
  * obtiene todos los miembros.
  */
-app.get('/api/get-miembros/', (req, res) => {
-    let miembros = db.collection('miembros')
-    miembros.get()
-        .then(snapshot => {
-            const miembros_res = snapshot.docs.map(doc => doc.data());
-            return res.status(200).json({ status:'ok', data:miembros_res });
-        })
-        .catch(err => {
-            console.log('Error getting document', err);
-        });
+app.get('/api/get-miembros/', async (req, res) => {
+    try {
+        const miembros = await db.collection('miembros').get()
+        const miembros_res = miembros.docs.map(doc => doc.data());
+        return res.status(200).json({ status:'ok', data:miembros_res });
+    } catch (err) {
+        console.log('Error getting document', err);
+        return res.status(500).json({ status:'error', error: err });
+    }
 });
 
 /**
  * @description
  * Actualiza un miembro con los datos introducidos a partir de de su id.
  */
-app.put('/api/update-miembro/:idMiembro', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('miembros').doc(req.params.idMiembro);
-            await document.set(req.body, {
-                merge: true
-            }).then(doc => {
-                return res.status(200).json({ status:'ok', data:doc });
-            });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({ status:'error', error: error });
-        }
-    })();
+app.put('/api/update-miembro/:idMiembro', async (req, res) => {
+    try {
+        const document = db.collection('miembros').doc(req.params.idMiembro);
+        const doc = await document.set(req.body, {
+            merge: true
+        });
+        return res.status(200).json({ status:'ok', data:doc });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status:'error', error: error });
+    }
 });
 
 /**
  * @description
  * Elimina un miembro a partir de su id.
  */
-app.delete('/api/delete-miembro/:idMiembro', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('miembros').doc(req.params.idMiembro);
-            await document.delete();
-            return res.status(200).json({ status:'ok' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({ status:'error', error: error });
-        }
-    })();
+app.delete('/api/delete-miembro/:idMiembro', async (req, res) => {
+    try {
+        const document = db.collection('miembros').doc(req.params.idMiembro);
+        await document.delete();
+        return res.status(200).json({ status:'ok' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status:'error', error: error });
+    }
 });
 
 exports.app = functions.https.onRequest(app);
